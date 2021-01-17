416a4e70d2e828bc5130a5b0a775fc76b399915f
Yury Cooliq <yurycooliq@gmail.com>
2021-01-17 17:47:33 +0200

    Router removed

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import Web3 from 'web3'
@@ -10,7 +9,6 @@ Vue.config.productionTip = false
 Vue.prototype.$web3 = window.ethereum ? new Web3(window.ethereum) : null
 
 new Vue({
-  router,
   store,
   vuetify,
   render: h => h(App)
