016d567c33b7f2a886fdfaf4bc4eceaaf3e0cf0f
Yury Cooliq <yurycooliq@gmail.com>
2021-01-17 17:45:11 +0200

    Global Web3

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import Web3 from 'web3'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$web3 = window.ethereum ? new Web3(window.ethereum) : null
 
 new Vue({
   router,
